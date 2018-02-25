2028fa698efba57ac48b42297555ca6c3c52df98
Halyna Mezhynska <galmezh@gmail.com>
2018-02-25 17:09:27 +0300

    Warmup function implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function warmup(temperature) {
-  // your implementation here
+module.exports = function warmup(celcium) {
+  const fahrenheit = celcium * 9 / 5 + 32;
+  return fahrenheit;
 };
